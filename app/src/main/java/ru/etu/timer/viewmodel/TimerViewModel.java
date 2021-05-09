64c130ea646744849522950728b2b3b541d4907c
Nikita Nechepurenko <rufflerock@gmail.com>
2021-05-09 10:58:30 +0300

    separate ui from data using viewmodel

--- app/src/main/java/ru/etu/timer/viewmodel/TimerViewModel.java
+++ app/src/main/java/ru/etu/timer/viewmodel/TimerViewModel.java
@@ -5,16 +5,30 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import ru.etu.timer.dto.TimeContainer;
+import ru.etu.timer.service.timer.Timer;
 
 public class TimerViewModel extends ViewModel {
-    MutableLiveData<TimeContainer> currentTimeOnClock = new MutableLiveData<>(new TimeContainer(0));
+    MutableLiveData<TimeContainer> currentTimeOnClock = new MutableLiveData<>();
+    MutableLiveData<Timer> currentWorkingTimer = new MutableLiveData<>();
 
-    public LiveData<TimeContainer> getCurrentTimeOnClock() {
+    public TimerViewModel() {
+        currentTimeOnClock.setValue(new TimeContainer(0));
+    }
+
+    public LiveData<TimeContainer> getCurrentTimeOnClockLiveData() {
         return currentTimeOnClock;
     }
 
     public void setCurrentTimeOnClock(TimeContainer timeContainer) {
         currentTimeOnClock.postValue(timeContainer);
+    }
+
+    public void setCurrentWorkingTimer(Timer timer) {
+        currentWorkingTimer.setValue(timer);
+    }
+
+    public Timer getCurrentWorkingTimer() {
+        return currentWorkingTimer.getValue();
     }
 
 }
